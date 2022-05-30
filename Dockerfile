7c54e460b420d59b769e84ab020d1a7bf6cd09d7
chooli <chooli.yip@gmail.com>
2022-05-30 12:17:48 -0400

    update lib

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Docker for Content Vault microserivce 
+FROM openjdk:11
+ARG env
+# local file storage path
+RUN mkdir -p /opt/domain-data/log
+COPY src/main/resources/application.${env}.properties /opt/domain-data/application.properties
+COPY target/domain-data-*.jar /opt/domain-data/domain-data.jar
+RUN ln -sf /dev/stdout /opt/domain-data/log/domain-data.sys.log
+
+CMD ["java", "-jar", "/opt/domain-data/domain-data.jar", "--spring.config.additional-location=/opt/domain-data/application.properties"]
+
+EXPOSE 8083
--- src/test/java/com/jumkid/domain/DomainDataApplicationTests.java
+++ src/test/java/com/jumkid/domain/DomainDataApplicationTests.java
@@ -1,13 +1,32 @@
 package com.jumkid.domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-class DomainDataApplicationTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
+public class DomainDataApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Before
+	public void contextLoads() {
+	}
 
 	@Test
-	void contextLoads() {
+	public void dummyTest(){
+		Assert.assertTrue(true);
 	}
 
 }
